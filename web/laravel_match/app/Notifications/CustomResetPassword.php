eda5a664441b75542f529561ca591c9902cbeee4
細川浩太 <k.hosokawa0716@gmail.com>
2020-05-16 14:22:12 +0900

    メール日本語化いったん終了

---
+++ web/laravel_match/app/Notifications/CustomResetPassword.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Notifications\Notification;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Auth\Notifications\ResetPassword;
+use Illuminate\Support\Facades\Lang;
+
+class CustomResetPassword extends Notification
+{
+
+    /**
+    * The password reset token.
+    *
+    /** @var string */
+    public $token;
+
+    use Queueable;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @param string token
+     * @return void
+     */
+    public function __construct($token)
+    {
+        $this->token = $token;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        $token = $this->token; // URL生成用に定義
+        $email = '?email='.urlencode($notifiable->getEmailForPasswordReset()); // URL生成用に定義
+
+        return (new MailMessage)
+                    ->from('k.hosokawa0716@gmail.com', config('app.name'))
+                    ->subject(Lang::getFromJson('パスワード再設定のご案内'))
+                    ->line(Lang::getFromJson('下のボタンをクリックして、パスワードを再設定してください。'))
+                    ->action(Lang::getFromJson('パスワード再設定'), url(config('app.url').'/password/reset/'.$token.$email))
+                    ->line(Lang::getFromJson('このリンクの有効期限は、 :count 分です。', ['count' => config('auth.passwords.'.config('auth.defaults.passwords').'.expire')]))
+                    ->line(Lang::getFromJson('もし心当たりがない場合は、本メッセージは破棄してください。'));
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+            //
+        ];
+    }
+}
--- web/laravel_match/app/User.php
+++ web/laravel_match/app/User.php
@@ -2,6 +2,7 @@
 
 namespace App;
 
+use App\Notifications\CustomResetPassword;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
@@ -50,4 +51,15 @@ class User extends Authenticatable
     {
         return $this->hasMany('App\Project');
     }
+
+    /**
+     * パスワード再設定メールの送信
+     *
+     * @param  string  $token
+     * @return void
+     */
+//    public function sendPasswordResetNotification($token)
+//    {
+//        $this->notify(new CustomResetPassword($token));
+//    }
 }
