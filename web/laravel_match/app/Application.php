f0506c9eae7c46f1a28b9418c0511e5e2958bd4a
細川浩太 <k.hosokawa0716@gmail.com>
2020-06-09 10:04:05 +0900

    整数型へのキャストを追加

--- web/laravel_match/app/Application.php
+++ web/laravel_match/app/Application.php
@@ -11,6 +11,15 @@ class Application extends Model
         'id', 'applicant', 'project_id', 'owner_id', 'applicant_id', 'created_at'
     ];
 
+    // DBが整数型であるが、本番環境ではstring型でとってきてしまうため追記
+    protected $casts = [
+        'id' => 'int',
+        'project_id' => 'int',
+        'owner_id' => 'int',
+        'applicant_id' => 'int',
+        'delete_flg' => 'int'
+    ];
+
     public function applicant()
     {
         return $this->belongsTo('App\User', 'applicant_id', 'id', 'users');
